14a4ef788213c145c8dc56ff08601862697b2c58
Rory Ashford <rory@superstrukt.co.uk>
2015-09-16 15:53:43 +0100

    Added aditional css class arguments

--- wp_bem_menu.php
+++ wp_bem_menu.php
@@ -104,16 +104,29 @@ class walker_texas_ranger extends Walker_Nav_Menu {
  * bem_menu returns an instance of the walker_texas_ranger class with the following arguments
  * @param  string $location This must be the same as what is set in wp-admin/settings/menus for menu location.
  * @param  string $css_class_prefix This string will prefix all of the menu's classes, BEM syntax friendly
+ * @param  arr/string $css_class_modifiers Provide either a string or array of values to apply extra classes to the <ul> but not the <li's>
  * @return [type]
  */
 
-function bem_menu($location = "main_menu", $css_class_prefix = 'main-menu'){  
+function bem_menu($location = "main_menu", $css_class_prefix = 'main-menu', $css_class_modifiers = null){  
+    
+    // Check to see if any css modifiers were supplied
+    if($css_class_modifiers){
+
+        if(is_array($css_class_modifiers)){
+            $modifiers = implode(" ", $css_class_modifiers);
+        } elseif (is_string($css_class_modifiers)) {
+            $modifiers = $css_class_modifiers;
+        }
+
+    } else {
+        $modifiers = '';
+    }
+
     $args = array(
         'theme_location'    => $location,
-        //'menu_class'        => $css_class_prefix,
         'container'         => false,
-        //'container_class'   => $css_class_prefix,
-        'items_wrap'        => '<ul class="' . $css_class_prefix . '">%3$s</ul>',
+        'items_wrap'        => '<ul class="' . $css_class_prefix . ' ' . $modifiers . '">%3$s</ul>',
         'walker'            => new walker_texas_ranger($css_class_prefix, true)
     );
     
