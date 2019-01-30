a3260a3ce72e682d37950945073fa87ac4df4a7c
Willemijn <willemijn@level-level.com>
2019-01-30 14:34:48 +0100

    add better validation and code format

--- wp_bem_menu.php
+++ wp_bem_menu.php
@@ -2,123 +2,149 @@
 
 /**
  * Walker Texas Ranger
- * Inserts some BEM naming sensibility into Wordpress menus
+ * Inserts some BEM naming sensibility into WordPress menus
  */
 
 class walker_texas_ranger extends Walker_Nav_Menu {
 
-    function __construct($css_class_prefix) {
+	public function __construct( $css_class_prefix ) {
 
-        $this->css_class_prefix = $css_class_prefix;
-        
-        // Define menu item names appropriately
+		$this->css_class_prefix = $css_class_prefix;
 
-        $this->item_css_class_suffixes = array(
-            'item'                      => '__item',  
-            'parent_item'               => '__item--parent',
-            'active_item'               => '__item--active',
-            'parent_of_active_item'     => '__item--parent--active',
-            'ancestor_of_active_item'   => '__item--ancestor--active',
-            'sub_menu'                  => '__sub-menu',
-            'sub_menu_item'             => '__sub-menu__item',
-            'link'                      => '__link',
-        );
+		// Define menu item names appropriately
 
-    }
+		$this->item_css_class_suffixes = array(
+			'item'                    => '__item',
+			'parent_item'             => '__item--parent',
+			'active_item'             => '__item--active',
+			'parent_of_active_item'   => '__item--parent--active',
+			'ancestor_of_active_item' => '__item--ancestor--active',
+			'sub_menu'                => '__sub-menu',
+			'sub_menu_item'           => '__sub-menu__item',
+			'link'                    => '__link',
+		);
 
-    // Check for children
+	}
 
-    function display_element( $element, &$children_elements, $max_depth, $depth=0, $args, &$output ){
-                
-        $id_field = $this->db_fields['id'];
-        
-        if ( is_object( $args[0] ) ) {
-            $args[0]->has_children = !empty( $children_elements[$element->$id_field] );
-        }
-        
-        return parent::display_element( $element, $children_elements, $max_depth, $depth, $args, $output );
-    
-    }
-    
-    function start_lvl(&$output, $depth = 1, $args=array()) {
-        
-        $real_depth = $depth + 1;
-        
-        $indent = str_repeat("\t", $real_depth);
+	// Check for children
 
-        $prefix = $this->css_class_prefix;
-        $suffix = $this->item_css_class_suffixes;
+	public function display_element( $element, &$children_elements, $max_depth, $depth = 0, $args, &$output ) {
 
-        $classes = array(
-            $prefix . $suffix['sub_menu'],
-            $prefix . $suffix['sub_menu']. '--' . $real_depth
-        );
+		$id_field = $this->db_fields['id'];
 
-        $class_names = implode( ' ', $classes );
+		if ( is_object( $args[0] ) ) {
+			$args[0]->has_children = ! empty( $children_elements[ $element->$id_field ] );
+		}
 
-        // Add a ul wrapper to sub nav
+		return parent::display_element( $element, $children_elements, $max_depth, $depth, $args, $output );
 
-        $output .= "\n" . $indent . '<ul class="'. $class_names .'">' ."\n";
-    }
-  
-    // Add main/sub classes to li's and links
-     
-    function start_el( &$output, $item, $depth = 0, $args = array(), $id = 0 ) {
-        
-        global $wp_query;
-        
-        $indent = ( $depth > 0 ? str_repeat( "    ", $depth ) : '' ); // code indent
+	}
 
-        $prefix = $this->css_class_prefix;
-        $suffix = $this->item_css_class_suffixes;
+	public function start_lvl( &$output, $depth = 1, $args = array() ) { 
 
-        // Item classes
-        $item_classes =  array(
-            'item_class'            => $depth == 0 ? $prefix . $suffix['item'] : '',
-            'parent_class'          => $args->has_children ? $parent_class = $prefix . $suffix['parent_item'] : '',
-            'active_page_class'     => in_array("current-menu-item",$item->classes) ? $prefix . $suffix['active_item'] : '',
-            'active_parent_class'   => in_array("current-menu-parent",$item->classes) ? $prefix . $suffix['parent_of_active_item'] : '',
-            'active_ancestor_class' => in_array("current-menu-ancestor",$item->classes) ? $prefix . $suffix['ancestor_of_active_item'] : '',
-            'depth_class'           => $depth >=1 ? $prefix . $suffix['sub_menu_item'] . ' ' . $prefix . $suffix['sub_menu'] . '--' . $depth . '__item' : '',
-            'item_id_class'         => $prefix . '__item--'. $item->object_id,
-            'user_class'            => $item->classes[0] !== '' ? $prefix . '__item--'. $item->classes[0] : ''
-        );
+		$real_depth = $depth + 1;
 
-        // convert array to string excluding any empty values
-        $class_string = implode("  ", array_filter($item_classes));
+		$indent = str_repeat( "\t", $real_depth );
 
-        // Add the classes to the wrapping <li>
-        $output .= $indent . '<li class="' . $class_string . '">';
+		$prefix = $this->css_class_prefix;
+		$suffix = $this->item_css_class_suffixes;
 
-        // Link classes
-        $link_classes = array(
-            'item_link'             => $depth == 0 ? $prefix . $suffix['link'] : '',
-            'depth_class'           => $depth >=1 ? $prefix . $suffix['sub_menu'] . $suffix['link'] . '  ' . $prefix . $suffix['sub_menu'] . '--' . $depth . $suffix['link'] : '',
-        );
+		$classes = array(
+			$prefix . $suffix['sub_menu'],
+			$prefix . $suffix['sub_menu'] . '--' . $real_depth,
+		);
 
-        $link_class_string = implode("  ", array_filter($link_classes));
-        $link_class_output = 'class="' . $link_class_string . '"';
+		$class_names = implode( ' ', $classes );
 
-        // link attributes
-        $attributes  = ! empty($item->attr_title) ? ' title="'  . esc_attr($item->attr_title) .'"' : '';
-        $attributes .= ! empty($item->target)     ? ' target="' . esc_attr($item->target    ) .'"' : '';
-        $attributes .= ! empty($item->xfn)        ? ' rel="'    . esc_attr($item->xfn       ) .'"' : '';
-        $attributes .= ! empty($item->url)        ? ' href="'   . esc_attr($item->url       ) .'"' : '';
+		// Add a ul wrapper to sub nav
 
-        // Creatre link markup
-        $item_output = $args->before;
-        $item_output .= '<a' . $attributes . ' ' . $link_class_output . '>';
-        $item_output .=     $args->link_before;
-        $item_output .=     apply_filters('the_title', $item->title, $item->ID);
-        $item_output .=     $args->link_after;
-        $item_output .=     $args->after;
-        $item_output .= '</a>';
+		$output .= "\n" . $indent . '<ul class="' . $class_names . '">' . "\n";
+	}
 
-        // Filter <li>
- 
-        $output .= apply_filters('walker_nav_menu_start_el', $item_output, $item, $depth, $args);
-    }
-    
+	// Add main/sub classes to li's and links
+
+	public function start_el( &$output, $item, $depth = 0, $args = array(), $id = 0 ) { 
+		global $wp_query;
+
+		$indent = ( $depth > 0 ? str_repeat( '    ', $depth ) : '' ); // code indent
+
+		$prefix = $this->css_class_prefix;
+		$suffix = $this->item_css_class_suffixes;
+
+		if ( is_object( $args ) && $args->has_children ) {
+			$parent_class = $prefix . $suffix['parent_item'];
+		} else {
+			$parent_class = '';
+		}
+
+		if ( is_array( $item->classes ) ) {
+			$active_page_class     = in_array( 'current-menu-item', $item->classes, true ) ? $prefix . $suffix['active_item'] : '';
+			$active_parent_class   = in_array( 'current-menu-parent', $item->classes, true ) ? $prefix . $suffix['parent_of_active_item'] : '';
+			$active_ancestor_class = in_array( 'current-menu-ancestor', $item->classes, true ) ? $prefix . $suffix['ancestor_of_active_item'] : '';
+			if ( isset( $item->classes[0] ) ) {
+				$user_class = '' !== $item->classes[0] ? $prefix . '__item--' . $item->classes[0] : '';
+			}
+		} else {
+			$active_page_class     = '';
+			$active_parent_class   = '';
+			$active_ancestor_class = '';
+			$user_class            = '';
+		}
+
+		// Item classes
+		$item_classes = array(
+			'item_class'            => 0 === $depth ? $prefix . $suffix['item'] : '',
+			'parent_class'          => $parent_class,
+			'active_page_class'     => $active_page_class,
+			'active_parent_class'   => $active_parent_class,
+			'active_ancestor_class' => $active_ancestor_class,
+			'depth_class'           => $depth >= 1 ? $prefix . $suffix['sub_menu_item'] . ' ' . $prefix . $suffix['sub_menu'] . '--' . $depth . '__item' : '',
+			'item_id_class'         => $prefix . '__item--' . $item->object_id,
+			'user_class'            => $user_class,
+		);
+
+		// convert array to string excluding any empty values
+		$class_string = implode( '  ', array_filter( $item_classes ) );
+
+		// Add the classes to the wrapping <li>
+		$output .= $indent . '<li class="' . $class_string . '">';
+
+		// Link classes
+		$link_classes = array(
+			'item_link'   => 0 === $depth ? $prefix . $suffix['link'] : '',
+			'depth_class' => $depth >= 1 ? $prefix . $suffix['sub_menu'] . $suffix['link'] . '  ' . $prefix . $suffix['sub_menu'] . '--' . $depth . $suffix['link'] : '',
+		);
+
+		$link_class_string = implode( '  ', array_filter( $link_classes ) );
+		$link_class_output = 'class="' . $link_class_string . '"';
+
+		// link attributes
+		$attributes  = ! empty( $item->attr_title ) ? ' title="' . esc_attr( $item->attr_title ) . '"' : '';
+		$attributes .= ! empty( $item->target ) ? ' target="' . esc_attr( $item->target ) . '"' : '';
+		$attributes .= ! empty( $item->xfn ) ? ' rel="' . esc_attr( $item->xfn ) . '"' : '';
+		$attributes .= ! empty( $item->url ) ? ' href="' . esc_attr( $item->url ) . '"' : '';
+
+		// Create link markup
+		if ( is_object( $args ) ) {
+			$item_output  = $args->before;
+			$item_output .= '<a' . $attributes . ' ' . $link_class_output . '>';
+			$item_output .= $args->link_before;
+			$item_output .= apply_filters( 'the_title', $item->title, $item->ID );
+			$item_output .= $args->link_after;
+			$item_output .= $args->after;
+			$item_output .= '</a>';
+		} else {
+			$item_output  = '';
+			$item_output .= '<a' . $attributes . ' ' . $link_class_output . '>';
+			$item_output .= apply_filters( 'the_title', $item->title, $item->ID );
+			$item_output .= '</a>';
+		}
+
+		// Filter <li>
+
+		$output .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
+	}
+
 }
 
 /**
@@ -129,31 +155,30 @@ class walker_texas_ranger extends Walker_Nav_Menu {
  * @return [type]
  */
 
-function bem_menu($location = "main_menu", $css_class_prefix = 'main-menu', $css_class_modifiers = null){  
-    
-    // Check to see if any css modifiers were supplied
-    if($css_class_modifiers){
+function bem_menu( $location = 'main_menu', $css_class_prefix = 'main-menu', $css_class_modifiers = null ) {
 
-        if(is_array($css_class_modifiers)){
-            $modifiers = implode(" ", $css_class_modifiers);
-        } elseif (is_string($css_class_modifiers)) {
-            $modifiers = $css_class_modifiers;
-        }
+	// Check to see if any css modifiers were supplied
+	if ( $css_class_modifiers ) {
 
-    } else {
-        $modifiers = '';
-    }
+		if ( is_array( $css_class_modifiers ) ) {
+			$modifiers = implode( ' ', $css_class_modifiers );
+		} elseif ( is_string( $css_class_modifiers ) ) {
+			$modifiers = $css_class_modifiers;
+		}
+	} else {
+		$modifiers = '';
+	}
 
-    $args = array(
-        'theme_location'    => $location,
-        'container'         => false,
-        'items_wrap'        => '<ul class="' . $css_class_prefix . ' ' . $modifiers . '">%3$s</ul>',
-        'walker'            => new walker_texas_ranger($css_class_prefix, true)
-    );
-    
-    if (has_nav_menu($location)){
-        return wp_nav_menu($args);
-    }else{
-        echo "<p>You need to first define a menu in WP-admin<p>";
-    }
+	$args = array(
+		'theme_location' => $location,
+		'container'      => false,
+		'items_wrap'     => '<ul class="' . $css_class_prefix . ' ' . $modifiers . '">%3$s</ul>',
+		'walker'         => new walker_texas_ranger( $css_class_prefix, true ),
+	);
+
+	if ( has_nav_menu( $location ) ) {
+		return wp_nav_menu( $args );
+	} else {
+		echo '<p>You need to first define a menu in WP-admin<p>';
+	}
 }
